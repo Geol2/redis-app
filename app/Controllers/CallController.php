ea861ad994e9cd06553881eddcff10c45b1b66cd
Geol2 <big9401@gmail.com>
2023-07-26 19:09:36 +0900

    ADD function : CallController::runTen()

--- app/Controllers/CallController.php
+++ app/Controllers/CallController.php
@@ -58,4 +58,19 @@ class CallController
         exit();
     }
 
+    public static function runTen() {
+        $_POST['time'] = 3600;
+        $time = new ExecTime(['run']);
+        $time->start();
+        $data = PostCurl::post("http://158.247.202.184:8080/run/ten", $_POST, null);
+        $time->end();
+        $run_time = $time->diff("run");
+
+        $decode = json_decode($data);
+        echo '<pre>';
+        echo $data.'<pre>';
+        echo "총 응답시간 : " . $run_time. " (sec)<pre>";
+        echo "네트워크 소요시간 : " .($run_time - $decode->spend_time)." (sec)<pre>";
+        exit();
+    }
 }
