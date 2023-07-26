3b656d84d337a2936f12b04b66a2e5b3e8418913
Geol2 <big9401@gmail.com>
2023-07-26 15:51:45 +0900

    ADD CallController::list(), search() : check total response time

--- app/Controllers/CallController.php
+++ app/Controllers/CallController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use WalnutBread\Curl\PostCurl;
+use WalnutBread\Time\ExecTime;
 
 class CallController
 {
@@ -11,18 +12,32 @@ class CallController
     }
 
     public static function search() {
+        $time = new ExecTime(['search_time']);
+        $time->start();
         $data = PostCurl::post("http://158.247.202.184:8080/keyword/search", $_POST, null);
+        $time->end();
+        $search_time = $time->diff("search_time");
 
+        $decode = json_decode($data);
         echo '<pre>';
-        echo $data;
+        echo $data.'<pre>';
+        echo "총 응답시간 : " . $search_time." (sec)<pre>";
+        echo "네트워크 소요시간 : " .($search_time - $decode->spend_time)." (sec)<pre>";
         exit();
     }
 
     public static function list() {
+        $time = new ExecTime(['list_time']);
+        $time->start();
         $data = PostCurl::post("http://158.247.202.184:8080/keyword/list", null, null);
-        
+        $time->end();
+        $list_time = $time->diff("list_time");
+
+        $decode = json_decode($data);
         echo '<pre>';
-        echo $data;
+        echo $data.'<pre>';
+        echo "총 응답시간 : " . $list_time. " (sec)<pre>";
+        echo "네트워크 소요시간 : " .($list_time - $decode->spend_time)." (sec)<pre>";
         exit();
     }
 }
