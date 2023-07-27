2c684bdab9ad3efd22944403aa9e4b2771297be1
Geol2 <big9401@gmail.com>
2023-07-27 19:18:16 +0900

    ADD exec_test File

---
+++ public/run.php
@@ -0,0 +1,16 @@
+<?php
+include_once './../vendor/autoload.php';
+
+use WalnutBread\Curl\PostCurl;
+use WalnutBread\Time\ExecTime;
+
+$get = $_GET;
+
+$time = new ExecTime(['run']);
+$time->start();
+$data = PostCurl::post("http://158.247.202.184:8080/run/time", $get, null);
+$time->end();
+$run_time = $time->diff("run");
+
+$decode = json_decode($data);
+var_dump($data);
