7a4a05fa73c6beb61817356f184d71f998d3f476
Geol2 <big9401@gmail.com>
2023-07-27 20:55:26 +0900

    Add Guzzle, run.php

--- public/run.php
+++ public/run.php
@@ -1,18 +1,18 @@
 <?php
 include_once './../vendor/autoload.php';
 
+use GuzzleHttp\Psr7\Request;
 use WalnutBread\Curl\PostCurl;
 use WalnutBread\Time\ExecTime;
 
-$get = [
-    "time" => 10
-];
-
 $time = new ExecTime(['run']);
 $time->start();
-$data = PostCurl::post("http://158.247.202.184:8080/run/time", $get, null);
+$client = new GuzzleHttp\Client();
+$request = new Request("POST", "http://localhost:9002/run/time?time=".$_GET['time']);
+$promise = $client->sendAsync($request)->then(function ($response) {
+    echo 'I completed!' .$response->getBody();
+});
+$promise->wait();
+
 $time->end();
 $run_time = $time->diff("run");
-
-$decode = json_decode($data);
-var_dump($data);
